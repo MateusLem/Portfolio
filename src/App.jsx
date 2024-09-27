9ad73b9d6e067cbdd5ead40896c86b0f2f508332
mateuslem <rm557803@fiap.com.br>
2024-09-26 22:45:12 -0300

    Usinig outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 
-export default function App()  {
+export default function App() {
   return (
     <div className="dark:bg-gray-800 dark:text-white min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Outlet />
       </main>
       <Footer />
     </div>
   );
-};
+}
+
 
