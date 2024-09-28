52fc8b432a2d5f5d7d7aa12f1bf397c4fed46b25
MateusLem <rm557803@fiap.com.br>
2024-09-28 11:05:53 -0300

    Correção input color

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,12 @@
 export default function Contact() {
-  const handleSubmit = (resp) => {
-    resp.preventDefault();
-    console.log({
-      name: resp.target.name.value,
-      email: resp.target.email.value,
-      message: resp.target.message.value,
-    });
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    const formData = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      message: event.target.message.value,
+    };
+    console.log(formData);
   };
 
   return (
@@ -17,20 +18,20 @@ export default function Contact() {
             type="text"
             name="name"
             placeholder="Nome"
-            className="block w-full p-2 border border-gray-300 rounded"
+            className="block w-full p-2 border border-gray-300 rounded text-black" // Set text color
             required
           />
           <input
             type="email"
             name="email"
             placeholder="Email"
-            className="block w-full p-2 border border-gray-300 rounded"
+            className="block w-full p-2 border border-gray-300 rounded text-black" // Set text color
             required
           />
           <textarea
             name="message"
             placeholder="Sua mensagem"
-            className="block w-full p-2 border border-gray-300 rounded"
+            className="block w-full p-2 border border-gray-300 rounded text-black" // Set text color
             required
           />
           <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">
